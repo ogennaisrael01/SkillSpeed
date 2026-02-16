7957f6603ca21af37939a29b1df797567a50ee6b
ogenna israel <ogennaisrale98@gmail.com>
2026-02-16 22:01:52 +0000

    prepare files for deplyment

--- src/core/settings/production.py
+++ src/core/settings/production.py
@@ -1,19 +1,14 @@
 from .base import *
 
+import dj_database_url
+
 DEBUG = env.bool("DEBUG")
 
 DATABASES = {
-    "default": {
-        "ENGINE": env("POSTGRES_ENGINE"),
-        "NAME": env("POSTGRES_NAME"),
-        "USER": env("POSTGRES_USER"),
-        "PASSWORD": env("POSTGRES_PASSWORD"),
-        "HOST": env("POSTGRES_HOST"),
-        "PORT": env.int("POSTGRES_PORT")
-    }
+    "default": dj_database_url.config(default=env("DATABASE_URL"))
 }
 
 ALLOWED_HOSTS = env("ALOOWED_HOSTS").split(",")
 
-CELERY_BROKER_URL = env("REDIS_LOCATION") + "/1"
-CELERY_RESULT_BACKEND = env("REDIS_LOCATION") + "/1"
+CELERY_BROKER_URL = env("REDIS_URL")
+CELERY_RESULT_BACKEND = env("REDIS_URL")
